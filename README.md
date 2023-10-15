b2e301b65d17b7107ee869782fe5decd3ba8609d
Habiba <uhabiba005@gmail.com>
2023-10-15 15:42:59 +0200

    Add prometheus endpoint

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ A practice project to learn prometheus.
 
 ### Set up a go project
 - Create a basic html file to print Hello World.
+- Create a prometheus endpoint using promtheus/promhttp module.This will automatically expose some built-in metrics.
--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import  (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
 	"net/http"
 )
 
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.ListenAndServe(":3000", nil)
+
+	// Prometheus endpoint
+	http.Handle("/prometheus", promhttp.Handler())
+	
+	fmt.Println("Serving requests on port 3000")
+	err := http.ListenAndServe(":3000", nil)
+	log.Fatal(err)
 }
+
