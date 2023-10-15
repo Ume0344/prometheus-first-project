1443983027b9cd43cc177e741d3bc81c5cc1ce54
Habiba <uhabiba005@gmail.com>
2023-10-15 19:55:47 +0200

    Increment the http request counter in premetheus

--- main.go
+++ main.go
@@ -3,14 +3,34 @@ package main
 import  (
 	"fmt"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus"
 	"log"
 	"net/http"
+	"html/template"
 )
 
+// Create a var of prometheus Counter
+var httpRequests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Number of get requests.",
+	},
+)
+
+// Increment the counter whenever :3000 is accessed
+func recordHttpRequests(w http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("./static/index.html")
+	t.Execute(w, "")
+	httpRequests.Inc()
+}
+
+// Register the httRequest counter
+func init() {
+	prometheus.Register(httpRequests)
+}
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./static")))
-
+	http.HandleFunc("/", recordHttpRequests)
 	// Prometheus endpoint
 	http.Handle("/prometheus", promhttp.Handler())
 	
@@ -18,4 +38,3 @@ func main() {
 	err := http.ListenAndServe(":3000", nil)
 	log.Fatal(err)
 }
-
